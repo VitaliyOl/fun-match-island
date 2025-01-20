7cf30423264484fe2fa94a57fb790415d354b99c
VitaliyOl <oliferchuk@gmail.com>
2025-01-20 12:11:22 +0200

    some fix

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,9 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const burgerMenu = document.querySelector('.burger-menu img');
-  const nav = document.querySelector('.nav');
+const burgerMenu = document.querySelector('.burger-menu');
+const nav = document.querySelector('.nav');
+const closeIcon = document.querySelector('.close-icon');
+const burgerIcon = document.querySelector('.burger-menu img:not(.close-icon)');
 
-  burgerMenu.addEventListener('click', () => {
-    burgerMenu.classList.toggle('active');
-    nav.classList.toggle('active');
-  });
+burgerMenu.addEventListener('click', () => {
+  nav.classList.toggle('active');
+
+  if (nav.classList.contains('active')) {
+    closeIcon.style.display = 'block';
+    burgerIcon.style.display = 'none';
+  } else {
+    closeIcon.style.display = 'none';
+    burgerIcon.style.display = 'block';
+  }
 });
