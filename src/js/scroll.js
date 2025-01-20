3ff744021955d27a3f67a92a3cc8803db2a8da08
Yuriy <>
2025-01-20 15:40:17 +0200

    проба 1 хедер

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,10 +1,10 @@
-window.addEventListener('scroll', () => {
-  const hero = document.getElementById('hero');
-  const header = document.querySelector('header');
+// window.addEventListener('scroll', () => {
+//   const hero = document.getElementById('hero');
+//   const header = document.querySelector('header');
 
-  if (window.scrollY > 100) {
-    header.style.backgroundColor = 'rgba(15, 102, 145, 0.8)';
-  } else {
-    header.style.backgroundColor = 'transparent';
-  }
-});
+//   if (window.scrollY > 100) {
+//     header.style.backgroundColor = 'rgba(15, 102, 145, 0.8)';
+//   } else {
+//     header.style.backgroundColor = 'transparent';
+//   }
+// });
