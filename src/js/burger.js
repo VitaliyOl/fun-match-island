54df32e3dffd1219520b4d90fe2966962b6d06a4
VitaliyOl <oliferchuk@gmail.com>
2025-01-22 21:17:02 +0200

    burger menu closed

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,8 +2,9 @@ const burgerMenu = document.querySelector('.burger-menu');
 const nav = document.querySelector('.nav');
 const closeIcon = document.querySelector('.close-icon');
 const burgerIcon = document.querySelector('.burger-menu img:not(.close-icon)');
+const navLinks = document.querySelectorAll('.nav-list a');
 
-burgerMenu.addEventListener('click', () => {
+const toggleMenu = () => {
   nav.classList.toggle('active');
 
   if (nav.classList.contains('active')) {
@@ -13,4 +14,14 @@ burgerMenu.addEventListener('click', () => {
     closeIcon.style.display = 'none';
     burgerIcon.style.display = 'block';
   }
+};
+
+burgerMenu.addEventListener('click', toggleMenu);
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    if (nav.classList.contains('active')) {
+      toggleMenu();
+    }
+  });
 });
